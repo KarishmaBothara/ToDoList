105d41040e1c0ed3ff0ebfdb97f8261243393d04
Karishma Bothara <karishma@Karishmas-MacBook-Pro.fritz.box>
2018-03-01 10:40:49 +1300

    Making to-do list contract specific to account/address

--- contract/ToDoList.sol
+++ contract/ToDoList.sol
@@ -1,34 +1,36 @@
 pragma solidity ^0.4.11;
 
 contract ToDoList {
+  mapping (address => mapping (uint => string)) private todoList; //mapping of addresses to mapping of position and data
   address public owner;
-  mapping (uint=>string) private todoList;
-  uint private itemsInList;
+ // mapping (uint=>string) private todoList;
+  mapping (address => uint) ownerListCount; // mapping of address to #noOfItemInList
 
   function ToDoList() {
     owner = msg.sender;
-    itemsInList = 0;
   }
 
 
-  function addItem(uint position, string data)  external{
-    todoList[position] = data;
-    itemsInList++;
+  function addItem(string data)  external{
+    uint currentPos = ownerListCount[msg.sender];
+    currentPos = currentPos + 1;
+    todoList[msg.sender][currentPos] = data;
+    ownerListCount[msg.sender] = currentPos;
   }
 
   function deleteItem(uint position) external {
-      todoList[position] = "";
+      todoList[msg.sender][position] = "";
   }
 
   function editItem(uint position, string newData)  external{
-        todoList[position] = newData;
+        todoList[msg.sender][position] = newData;
   }
 
   function getItemAtPos(uint position) public view returns (string item){
-        return todoList[position];
+        return todoList[msg.sender][position];
   }
 
   function getItemsInList() public view returns (uint itemCount) {
-        return itemsInList;
+        return ownerListCount[msg.sender];
   }
 }
